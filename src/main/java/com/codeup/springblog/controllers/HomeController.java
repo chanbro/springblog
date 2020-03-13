9b8d3a0bd2f98b0e31435739f3e239146c989bb4
Chaney <52712847+chanbro@users.noreply.github.com>
2020-03-13 11:18:34 -0500

    application loading properly

---
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "landing";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
