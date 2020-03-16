2e267adc9f7ffbbce7e8ecf5283c97ff8da3f769
Chaney <52712847+chanbro@users.noreply.github.com>
2020-03-16 16:44:48 -0500

    added db exercises

---
+++ seeder.sql
@@ -0,0 +1,6 @@
+use springblog_db;
+
+INSERT INTO users (username, email, password)
+VALUES ('test',
+        'test@test.com',
+        'password');
---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,46 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length = 200)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+
+    public Post(long id, String title, String description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
