09c17c2e4abe8c6d1aafbcc7940f2236f8b09132
Chaney <52712847+chanbro@users.noreply.github.com>
2020-03-24 11:06:35 -0500

    Update seeder.sql

--- seeder.sql
+++ seeder.sql
@@ -5,10 +5,10 @@ insert into users (id, username, email, password) values (1, 'seusden0', 'smcgre
 insert into users (id, username, email, password) values (2, 'lclewley1', 'hhyndley1@dailymail.co.uk', '8AblpPIOYZ6');
 insert into users (id, username, email, password) values (3, 'cpavinese2', 'afotheringham2@opensource.org', '2cRle6CxWRog');
 
-insert into posts (id, title, body) values (1, 'Indian red admiral', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.');
-insert into posts (id, title, body) values (2, 'Mexican beaded lizard', 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.');
-insert into posts (id, title, body) values (3, 'Shark blue', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.');
-insert into posts (id, title, body) values (4, 'Bald Eagle', 'Luctus necaecenas, condimentum aliquam convallis nunc. Proin Pellentesque viverra pede ac diam. Suspendisse potenti pellentesque volutpat dui.');
+insert into posts (id, title, body, user_id) values (1, 'Indian red admiral', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 1);
+insert into posts (id, title, body, user_id) values (2, 'Mexican beaded lizard', 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.', 2);
+insert into posts (id, title, body, user_id) values (3, 'Shark blue', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 2);
+insert into posts (id, title, body, user_id) values (4, 'Bald Eagle', 'Luctus necaecenas, condimentum aliquam convallis nunc. Proin Pellentesque viverra pede ac diam. Suspendisse potenti pellentesque volutpat dui.', 3);
 
 
 
